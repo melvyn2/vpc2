734c7fc5d24f6d794760d01f98a83d49f90b9cba
Jeremy Lorelli <jeremy.lorelli.1337@gmail.com>
2020-07-19 01:21:42 -0700

    Missed uint32_t

--- utils/vpc/solutiongenerator_xcode.cpp
+++ utils/vpc/solutiongenerator_xcode.cpp
@@ -17,6 +17,7 @@
 #define snprintf _snprintf
 typedef unsigned long long uint64_t;
 typedef unsigned short uint16_t;
+typedef unsigned int uint32_t;
 typedef unsigned char uint8_t;
 typedef signed short int16_t;
 #else
